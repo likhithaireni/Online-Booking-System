a9d3271b636e0a5dce885bd125aaf22510d89239
likhithaireni <irenilikhitha@gmail.com>
2025-04-07 19:50:19 +0530

    Added base path

--- event-booking-frontend/vite.config.js
+++ event-booking-frontend/vite.config.js
@@ -9,6 +9,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '/Online-Booking-System/',
   plugins: [react()],
   server: {
     proxy: {
